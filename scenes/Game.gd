2ab75c883bade57740a7722011bdfa7d496d8b97
Anwarus <Anwarus1289@gmail.com>
2018-10-07 18:09:45 +0200

    gui instancing

--- scenes/Game.gd
+++ scenes/Game.gd
@@ -3,12 +3,23 @@ extends Node2D
 var track1 = preload("res://scenes/Track.tscn")
 var player1= preload("res://scenes/Car.tscn")
 
+var GUI = preload("res://scenes/GUI.tscn")
+var GUIPlayer = preload("res://scenes/GUIPlayer.tscn")
+
 func _ready():
     add_child(track1.instance())
     $Players.add_child(player1.instance())
+    
+    _setup_gui()
 
 func _process(delta):
     pass
         
 func checkpoint_reached(area):
     print(area.get_owner())
+    
+func _setup_gui():
+    add_child(GUI.instance())
+    
+    for player in $Players.get_children():
+        $GUI.add_child(GUIPlayer.instance())
