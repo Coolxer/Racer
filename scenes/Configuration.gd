53c327cb0cd75eae597d1e391c6fe09398fd5e7b
Anwarus <Anwarus1289@gmail.com>
2019-01-07 19:07:14 +0100

    (improvement) - show selected players icons

--- scenes/Configuration.gd
+++ scenes/Configuration.gd
@@ -1,4 +1,10 @@
 extends VBoxContainer
 
+func _ready():
+    $RoundsButton.add_item("1")
+    $RoundsButton.add_item("2")
+    $RoundsButton.add_item("3")
+    $RoundsButton.add_item("4")
+
 func _on_Back_pressed():
     Manager.load_scene(Manager.scenes["menu"])
