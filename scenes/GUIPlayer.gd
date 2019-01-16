e49f4e00e73bab1ddf1dd18d1c49d61067e81669
Anwarus <Anwarus1289@gmail.com>
2019-01-16 19:47:55 +0100

    (feature) - position ranking number based on checkpoints passed

--- scenes/GUIPlayer.gd
+++ scenes/GUIPlayer.gd
@@ -1,5 +1,8 @@
 extends HBoxContainer
 
+func update_position():
+    $Position.text = str(get_index() + 1) + "."
+
 func set_nickname(nickname):
     $Name.text = nickname
     
--- scenes/Players.gd
+++ scenes/Players.gd
@@ -7,16 +7,12 @@ func register_player(player_node):
     add_child(player_node.GUI_player_node)
 
 func update_order():
-#    var player = get_child(1)
-#    print(player)
-#    move_child(player, 0)
-    print(player_nodes)
     player_nodes.sort_custom(self, "_sort_players")
-    print(player_nodes)
     
     var index = 0
     for player_node in player_nodes:
         move_child(player_node.GUI_player_node, index)
+        player_node.GUI_player_node.update_position()
         index += 1 
           
 func _sort_players(player_1, player_2):
